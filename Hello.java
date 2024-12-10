80617e343a3b9a4ad9c8c8cb4a9d8c50ec6e3ed0
nithin <koppulanithinreddy@gmail.com>
2024-12-10 15:19:49 -0500

    latest commit for Hello.java

--- Hello.java
+++ Hello.java
@@ -1,6 +1,6 @@
 // This is a simple Java program that prints "Hello, World!" to the console.
 
-public class HelloWorld { 
+public class Hello { 
 
     public static void main(String[] args) { 
         System.out.println("Hello, World2!"); 
